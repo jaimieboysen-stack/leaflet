04ff02b1da8d99524d6715b18eaf4d7189bc0394
Jaimie Boysen <jaimie.boysen@mail.mcgill.ca>
2026-02-20 19:57:52 -0500

    updated

--- js/map.js
+++ js/map.js
@@ -1,57 +1,57 @@
 
-// globals
-let data = [
-	{
-		'title':'Osaka',
-		'lat': 34.6937,
-		'lon': 135.5023
-	},
-	{
-		'title':'Cali',
-		'lat': 3.4516,
-		'lon': -76.5320
-	},
-	{
-		'title':'Bangkok',
-		'lat': 13.7563,
-		'lon': 100.5018
-	},
-	{
-		'title':'Tokyo',
-		'lat': 35.6762,
-		'lon': 139.6503
-	},
-	{
-		'title':'LA',
-		'lat': 34.0522,
-		'lon': -118.2437
-	}
-]
-
-//create feature group 
-let myMarkers = L.featureGroup();
-
-var map = L.map('map').setView([34.0697,-118.4432], 17);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 
 
-// loop through data
-data.forEach(function(item){
-	// create marker
-	let marker = L.marker([item.lat,item.lon]).bindPopup(item.title)
+// Global variables
+let map;
+// path to csv data
+let path = "data/dunitz.csv";
+let markers = L.featureGroup();
 
-	// add marker to featuregroup
-	myMarkers.addLayer(marker)
+// initialize
+$( document ).ready(function() {
+	createMap();
+	readCSV(path);
+});
 
-	// add data to sidebar
-	$('.sidebar').append('<div class="sidebar-item">'+item.title+'</div>')
-})
+// create the map
+function createMap(){
+	map = L.map('map').setView([0,0],3);
 
-// after loop, add the FeatureGroup to map
-myMarkers.addTo(map)
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
 
-// zoom to the extent of all markers
-map.fitBounds(myMarkers.getBounds())
+// function to read csv data
+function readCSV(){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+
+//map csv
+function mapCSV(data){
+	
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create marker
+		let marker = L.marker([item.latitude,item.longitude])
+
+		// add marker to featuregroup
+		markers.addLayer(marker)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(markers.getBounds())
+}
